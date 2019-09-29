9ee8b534927250e8bfe235afbb349d5109c349bf
Jake Henson <jakegatsby935@gmail.com>
2019-09-28 23:47:22 -0600

    Updated with new files, continued work on game. Added a placeholder

---
+++ shmup/Assets/Scripts/HeroScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+static public HeroScript S; // Singleton
+
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    // Ship status information
+    public float shieldLevel = 1;
+    public bool ____________________________;
+    void Awake() {
+        S = this; // Set the Singleton
+    }
+    void Update () {
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal"); // 1
+        float yAxis = Input.GetAxis("Vertical"); // 1
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+        
+        // Rotate the ship to make it feel more dynamic // 2
+        transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
+    }
+}
--- shmup/Process.md
+++
@@ -1,6 +0,0 @@
-# Process
-
-### 9-23-19
-
-Committted Initial Repo, started work on game 
-____
